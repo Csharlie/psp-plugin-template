f2e9f1d069897744f74d99bfe959caa744cdf05e
Peter Sardy <petersardy@gmail.com>
2015-10-27 23:41:38 +0100

    Gulp-Concat Basics

---
+++ assets/js/example/example-three.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+	// Example three for gulp-concat
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,32 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+var config = {
+	sassSrc: './assets/sass',
+	cssDest: './dist/css',
+	jsSrc: './assets/js',
+	jsDest: './dist/js',
+	bowerDir: './bower_components'
+}
 
 gulp.task('sass', function(){
   return gulp
-  	.src('./assets/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist/css'))
+  	.src(config.sassPath + '/**/*.scss')
+    .pipe(sass({
+    	outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest(config.cssDest))
 });
 
-//Default task
-gulp.task('default',function() {
+gulp.task('concat', function(){
+  return gulp
+  	.src(config.jsSrc + '/**/*.js')
+  	.pipe(concat('all.js'))
+  	.pipe(gulp.dest(config.jsDest))
+});
+
+//Watch task
+gulp.task('watch',function() {
     gulp.watch('./assets/sass/**/*.scss',['sass']);
 });
