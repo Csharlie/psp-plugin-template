2123ad3e1ee9057138ac2f6e22679284abd6a690
Peter Sardy <petersardy@gmail.com>
2015-10-28 18:06:48 +0100

    Gulp Uglify Added

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minify = require('gulp-minify-css');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglifyjs');
 
 var config = {
+
+	// Stylesheets
 	sassSrc: './assets/sass',
 	cssDest: './dist/css',
+
+	// Javascripts
 	jsSrc: './assets/js',
 	jsDest: './dist/js',
+
+	// Bower
 	bowerDir: './bower_components'
 }
+
+/*
+ * Gulp Sass
+ */
 
 gulp.task('sass', function(){
   return gulp
@@ -19,11 +31,26 @@ gulp.task('sass', function(){
     .pipe(gulp.dest(config.cssDest))
 });
 
+/*
+ * Gulp Concat
+ */
+
 gulp.task('concat', function(){
   return gulp
   	.src(config.jsSrc + '/**/*.js')
   	.pipe(concat('all.js'))
   	.pipe(gulp.dest(config.jsDest))
+});
+
+/*
+ * Gulp UglifyJS
+ */
+
+gulp.task('compress', function() {
+  return gulp
+  	.src(config.jsSrc + '/**/*.js')
+    .pipe(uglify('all.min.js'))
+    .pipe(gulp.dest(config.jsDest));
 });
 
 //Watch task
